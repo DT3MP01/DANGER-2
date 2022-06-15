711e3aea05d843dff8652af691b90e75dadbf099
DT3MP01 <albertoalvaza@gmail.com>
2022-06-15 12:35:40 +0200

    Guardar con Storage Firebase

--- DANGER/Assets/Adrian/Scripts/LevelCreation/DBManager.cs
+++ DANGER/Assets/Adrian/Scripts/LevelCreation/DBManager.cs
@@ -1,10 +1,14 @@
 using Firebase;
 using Firebase.Firestore;
+using Firebase.Storage;
 using UnityEngine;
 using System.IO;
 using System.Collections.Generic;
+using System;
 using System.Collections;
+using System.Net.Http;
 using System.Linq;
+using System.Text;
 
 public class DBManager : MonoBehaviour
 {
@@ -18,6 +22,7 @@ public class DBManager : MonoBehaviour
     public ObjectCreation game;
     public timer timer;
     private FirebaseFirestore database;
+    private FirebaseStorage storage;
 
 
     // Start is called before the first frame update
@@ -31,19 +36,28 @@ public class DBManager : MonoBehaviour
         //reference = FirebaseDatabase.DefaultInstance.RootReference;
 
         database = FirebaseFirestore.DefaultInstance;
+        storage= FirebaseStorage.DefaultInstance;
+
     }
 
     IEnumerator SaveRoom()
     {
         yield return StartCoroutine(game.RecordFrame());
 
-        DocumentReference docData = database.Collection("GameData").Document();
-        docData.SetAsync(new Dictionary<string, object> {{"Raw",game.json}});
+        // DocumentReference docData = database.Collection("GameData").Document();
+        // docData.SetAsync(new Dictionary<string, object> {{"Raw",game.json}});
 
         DocumentReference docRefInfo = database.Collection("GameInfo").Document();
-        //StatsRoom statsRoom = new StatsRoom(game.meters, game.extinguishers, game.windows, game.doors, game.countScans);
+
+        StorageReference docData = storage.RootReference.Child("GameInfo-Data").Child(docRefInfo.Id);
+        docData.PutBytesAsync(Encoding.Unicode.GetBytes(game.json));
+
+        StorageReference docDataImage = storage.RootReference.Child("GameInfo-Data").Child(docRefInfo.Id+"-Image");
+        docDataImage.PutBytesAsync(Convert.FromBase64String(game.SaveRoomData.image));
+
+        StatsRoom statsRoom = new StatsRoom(game.meters, game.extinguishers, game.windows, game.doors, game.countScans);
         Dictionary<string, object>data =  new Dictionary<string, object> {
-            {"Image", game.SaveRoomData.image},
+            {"Image",docDataImage.Path},
             {"PlayerName", "PlayerExample"},
             {"RoomName", "ROOM"},
             {"Meters",game.SaveRoomData.statsRoom.meters},
@@ -51,9 +65,10 @@ public class DBManager : MonoBehaviour
             {"Windows",game.SaveRoomData.statsRoom.windows},
             {"Doors",game.SaveRoomData.statsRoom.doors},
             {"CountScans",game.SaveRoomData.statsRoom.countScans},
-            {"Reference", docData}
+            {"Reference",docData.Path}
             };
         docRefInfo.SetAsync(data);
+
     }
 
 

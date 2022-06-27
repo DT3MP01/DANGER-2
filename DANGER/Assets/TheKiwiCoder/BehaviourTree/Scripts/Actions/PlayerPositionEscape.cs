8937192d1cda50c7c7c7c80bbe16a156343b13fc
DT3MP01 <albertoalvaza@gmail.com>
2022-06-28 01:11:39 +0200

    BehavioursTrees

--- DANGER/Assets/TheKiwiCoder/BehaviourTree/Scripts/Actions/PlayerPositionEscape.cs
+++ DANGER/Assets/TheKiwiCoder/BehaviourTree/Scripts/Actions/PlayerPositionEscape.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TheKiwiCoder;
 
-public class PlayerPosition : ActionNode
+public class PlayerPositionEscape : ActionNode
 {
     public GameObject player;
 
